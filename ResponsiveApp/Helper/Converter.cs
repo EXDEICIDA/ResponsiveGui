d2dfd2ecbf89d19cb2be61e8b2feb06868c6b0d5
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-04-05 15:13:15 +0300

    Add files via upload

---
+++ ResponsiveApp/Helper/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace ResponsiveApp.Helper
+{
+   //IsLessThanConverter
+
+    public class IsLessThanConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instamce = new IsLessThanConverter();
+        public object Convert (object value,Type targetType,object parameter,CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble (value);
+            double compareToValue = System.Convert.ToDouble (parameter);
+            return doubleValue < compareToValue;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    //IsGreaterThanConverter
+
+    public class IsGreaterThanConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instamce = new IsGreaterThanConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+            return doubleValue > compareToValue;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
